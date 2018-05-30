d1d255302e733ac74fe576f1f3a336e3fdeac96a
saurabh <saurabh.karna@mountblue.io>
2018-05-30 19:14:14 +0530

    add stack graph for matches won

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function getYourFile(yourFile) {
 };
 
 
-$('#show-chart').click(function () {
+$('#show-mat-per-seas').click(function () {
   // console.log(JSON.stringify(matchesJson));
   let matchPerSeason = {};
   for (let i = 0; i < matchesJson.length; i++) {
@@ -50,7 +50,7 @@ $('#show-chart').click(function () {
   let dataFeed = [];
   let dataNames = [];
   for (const key of Object.keys(matchPerSeason)) {
-    dataFeed.push({"name": key, "y": matchPerSeason[key]})
+    dataFeed.push({ "name": key, "y": matchPerSeason[key] })
   }
   console.log(dataFeed)
 
@@ -71,6 +71,102 @@ $('#show-chart').click(function () {
       }
     ]
   });
+})
+
+$('#show-won-stack').click(function () {
+  // console.log(JSON.stringify(matchesJson));
+  let matchesWonPerSeason = {};
+  for (let i = 0; i < matchesJson.length; i++) {
+    let winner = matchesJson[i].winner;
+    if (winner == "Rising Pune Supergiants") {
+      winner = "Rising Pune Supergiant"
+    }
+    if (winner == "") {
+      winner = "No Result"
+    }
+    let year = matchesJson[i].season;
+    if (year in matchesWonPerSeason) {
+      if (matchesWonPerSeason[year][winner]) {
+        matchesWonPerSeason[year][winner] = matchesWonPerSeason[year][winner] + 1;
+      } else {
+        matchesWonPerSeason[year][winner] = 1;
+      }
+    } else {
+      matchesWonPerSeason[year] = {};
+      matchesWonPerSeason[year][winner] = 1;
+    }
+  }
+  console.log(matchesWonPerSeason);
+  let dataFeed = [];
+  let years = Object.keys(matchesWonPerSeason);
+  let dataTeamNames = [];
+  for (const key of years) {
+    for (const subkey of Object.keys(matchesWonPerSeason[key])) {
+      if (!dataTeamNames.includes(subkey)) {
+        dataTeamNames.push(subkey);
+      }
+    }
+  }
+
+  for (let i = 0; i < years.length; i++) {
+    dataFeed.push({ "name": years[i], "data": [] });
+    for (let j = 0; j < dataTeamNames.length; j++) {
+      let team = dataTeamNames[j];
+      if (matchesWonPerSeason[years[i]][team]) {
+        dataFeed[i]["data"].push(matchesWonPerSeason[years[i]][team]);
+      } else {
+        dataFeed[i]["data"].push(0);
+      }
+    }
+  }
+
+  Highcharts.chart('container', {
+    chart: { type: 'column' },
+    title: { text: 'IPL Analysis' },
+    subtitle: { text: 'Matches per Season' },
+    xAxis: {
+      categories: dataTeamNames
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Matches Won'
+      },
+      stackLabels: {
+        enabled: true,
+        style: {
+          fontWeight: 'bold',
+          color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+        }
+      }
+    },
+    legend: {
+      align: 'right',
+      x: -30,
+      verticalAlign: 'top',
+      y: 25,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+      borderColor: '#CCC',
+      borderWidth: 1,
+      shadow: false
+    },
+    tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: true,
+          color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+        }
+      }
+    },
+    series: dataFeed
+  });
+
 })
 
 
