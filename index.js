5c6e87902c89f290706923f7a1ab06981499e73e
saurabh <saurabh.karna@mountblue.io>
2018-06-09 10:45:39 +0530

    refactor code with array utilities

--- index.js
+++ index.js
@@ -1,3 +1,81 @@
+function createChart(xNames, dataFeed, xTitle, yTitle, subTitle, pt='{point.y:%f}') {
+  Highcharts.chart('container', {
+    chart: { type: 'column' },
+    title: { text: 'IPL Analysis' },
+    subtitle: { text: subTitle },
+    yAxis: { title: { text: yTitle } },
+    xAxis: {
+      categories: xNames,
+      title: { text: xTitle }
+    },
+    plotOptions: {
+      series: {
+        borderWidth: 0,
+        dataLabels: {
+          enabled: true,
+          format: pt
+        }
+      }
+    },
+    "series": [
+      {
+        "data": dataFeed,
+        name: "Matches",
+        colorByPoint: true,
+        showInLegend: false
+      }
+    ]
+  });
+}
+
+function createStackChart(xNames, dataFeed, xTitle, yTitle, subTitle) {
+  Highcharts.chart('container', {
+    chart: { type: 'column' },
+    title: { text: 'IPL Analysis' },
+    subtitle: { text: subTitle },
+    xAxis: {
+      categories: xNames,
+      title: { text: xTitle }
+    },
+    yAxis: {
+      min: 0,
+      title: { text: yTitle },
+      stackLabels: {
+        enabled: true,
+        style: {
+          fontWeight: 'bold',
+          color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+        }
+      }
+    },
+    legend: {
+      align: 'right',
+      x: -30,
+      verticalAlign: 'top',
+      y: 25,
+      floating: true,
+      backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+      borderColor: '#CCC',
+      borderWidth: 1,
+      shadow: false
+    },
+    tooltip: {
+      headerFormat: '<b>{point.x}</b><br/>',
+      pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal',
+        dataLabels: {
+          enabled: false,
+          color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+        }
+      }
+    },
+    series: dataFeed
+  });
+}
+
 $(document).ready(() => {
   let matchesJson;
   let deliveriesJson;
@@ -44,11 +122,11 @@ $(document).ready(() => {
   // --------------------------------------------------------
   //using async-await
   prepareData().then(() => {
-    $('p').attr("hidden","");
+    $('p').attr("hidden", "");
     $('div').removeAttr("hidden");
   }).catch(err => console.error(err));
 
-  async function prepareData(){
+  async function prepareData() {
     let matResponse = await fetch("CSVs/matches.csv");
     matchesJson = toJson(await matResponse.text());
     let delResponse = await fetch("CSVs/deliveries.csv");
@@ -57,276 +135,129 @@ $(document).ready(() => {
   }
   // -------------------------------------------------------
 
-  $('#show-mat-per-seas').click(function () {
-    let matchPerSeason = {};
-    matchesJson.forEach(elem => {
+  function getMatchSeason() {
+    return matchesJson.reduce((acc, elem) => {
       let year = elem.season;
-      if (year in matchPerSeason) {
-        matchPerSeason[year] = matchPerSeason[year] + 1;
-      } else {
-        matchPerSeason[year] = 1;
-      }
-    });
+      (year in acc) ? acc[year]++ : acc[year] = 1;
+      return acc;
+    }, {});
+  }
 
-    let dataFeed = [];
-    for (const key in matchPerSeason) {
-      dataFeed.push({ "name": key, "y": matchPerSeason[key] })
-    }
-
-    // Create the chart
-    Highcharts.chart('container', {
-      chart: { type: 'column' },
-      title: { text: 'IPL Analysis' },
-      subtitle: { text: 'Matches per Season' },
-      yAxis: { title: { text: 'Matches' } },
-      xAxis: {
-        categories: Object.keys(matchPerSeason),
-        title: { text: "Year" }
-      },
-      plotOptions: {
-        series: {
-          borderWidth: 0,
-          dataLabels: {
-            enabled: true,
-            format: '{point.y:%f}'
-          }
-        }
-      },
-      "series": [
-        {
-          "data": dataFeed,
-          name: "Matches",
-          colorByPoint: true,
-          showInLegend: false
-        }
-      ]
+  $('#show-mat-per-seas').click(function () {
+    let matchPerSeason = getMatchSeason();
+    let dataFeed = Object.keys(matchPerSeason).map(year => {
+      return { "name": year, "y": matchPerSeason[year] };
     });
+    createChart(Object.keys(matchPerSeason), dataFeed, "Year", "Matches", 'Matches per Season');
   })
 
   $('#show-won-stack').click(function () {
-    let matchesWonPerSeason = {};
-    matchesJson.forEach(elem => {
-      let winner = elem.winner;
-      if (winner == "Rising Pune Supergiants") {
-        winner = "Rising Pune Supergiant"
-      }
-      if (winner == "") {
-        winner = "No Result"
-      }
-      let year = elem.season;
-      if (year in matchesWonPerSeason) {
-        if (matchesWonPerSeason[year][winner]) {
-          matchesWonPerSeason[year][winner] = matchesWonPerSeason[year][winner] + 1;
-        } else {
-          matchesWonPerSeason[year][winner] = 1;
-        }
+    let winsPerSeason = matchesJson.reduce((acc, match) => {
+      let winner = match.winner;
+      if (winner == "Rising Pune Supergiants") { winner = "Rising Pune Supergiant" }
+      if (winner == "") { winner = "No Result" }
+
+      let year = match.season;
+      if (year in acc) {
+        (acc[year][winner]) ? acc[year][winner]++ : acc[year][winner] = 1;
       } else {
-        matchesWonPerSeason[year] = {};
-        matchesWonPerSeason[year][winner] = 1;
+        acc[year] = {};
+        acc[year][winner] = 1;
       }
-    });
-    let dataFeed = [];
-    let years = Object.keys(matchesWonPerSeason);
-    let dataTeamNames = [];
-    years.forEach(key => {
-      for (const subkey in matchesWonPerSeason[key]) {
-        if (!dataTeamNames.includes(subkey)) {
-          dataTeamNames.push(subkey);
-        }
-      }
-    });
+      return acc;
+    }, {});
 
-    for (const i in years) {
-      dataFeed.push({ "name": years[i], "data": [] });
-      for (const j in dataTeamNames) {
-        let team = dataTeamNames[j];
-        if (matchesWonPerSeason[years[i]][team]) {
-          dataFeed[i]["data"].push(matchesWonPerSeason[years[i]][team]);
+    let years = Object.keys(winsPerSeason);
+
+    let dataTeamNames = years.reduce((acc, yr) => {
+      Object.keys(winsPerSeason[yr]).forEach(team => {
+        if (!acc.includes(team)) {
+          acc.push(team);
+        }
+      });
+      return acc;
+    }, []);
+
+    let dataFeed = years.reduce((acc, yr, i) => {
+      acc.push({ "name": yr, "data": [] });
+      dataTeamNames.forEach(team => {
+        if (winsPerSeason[yr][team]) {
+          acc[i]["data"].push(winsPerSeason[yr][team]);
         } else {
-          dataFeed[i]["data"].push(0);
+          acc[i]["data"].push(0);
         }
-      }
-    }
-    // create chart
-    Highcharts.chart('container', {
-      chart: { type: 'column' },
-      title: { text: 'IPL Analysis' },
-      subtitle: { text: 'Matches won per year' },
-      xAxis: {
-        categories: dataTeamNames,
-        title: { text: "Teams" }
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Matches Won'
-        },
-        stackLabels: {
-          enabled: true,
-          style: {
-            fontWeight: 'bold',
-            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
-          }
-        }
-      },
-      legend: {
-        align: 'right',
-        x: -30,
-        verticalAlign: 'top',
-        y: 25,
-        floating: true,
-        backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
-        borderColor: '#CCC',
-        borderWidth: 1,
-        shadow: false
-      },
-      tooltip: {
-        headerFormat: '<b>{point.x}</b><br/>',
-        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-      },
-      plotOptions: {
-        column: {
-          stacking: 'normal',
-          dataLabels: {
-            enabled: false,
-            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
-          }
-        }
-      },
-      series: dataFeed
-    });
+      });
+      return acc;
+    }, []);
 
+    createStackChart(dataTeamNames, dataFeed, "Teams", "Matches Won", "Matches won per year");
   })
 
   $('#show-extra-runs').click(function () {
-    let extraRunsPerTeam = {};
-    let matchNoStart16;
-    let matchNoEnd16;
-    let yearDeliveriesData = [];
-    let sum = 0;
-    let matchPerSeason = {};
+    let matchPerSeason = getMatchSeason();
 
-    matchesJson.forEach(elem => {
-      let year = elem.season;
-      if (year in matchPerSeason) {
-        matchPerSeason[year] = matchPerSeason[year] + 1;
-      } else {
-        matchPerSeason[year] = 1;
-      }
-    });
+    let sum = Object.keys(matchPerSeason).reduce((sum, year) => {
+      if (year <= 2016) { return sum + matchPerSeason[year]; }
+      return (year <= 2016) ? sum + matchPerSeason[year] : sum;
+    }, 0)
 
-    for (const yr in matchPerSeason) {
-      if (yr <= 2016) {
-        sum += matchPerSeason[yr];
-      }
-    }
     //since last season(2017) is at the top, Add 2017 matches to sum
     // replace 2017 with current season for other times
     sum += matchPerSeason[2017];
-    matchNoEnd16 = sum;
-    matchNoStart16 = sum - matchPerSeason[2016] + 1;
+    let matchNoEnd16 = sum;
+    let matchNoStart16 = sum - matchPerSeason[2016] + 1;
 
-    for (const key in deliveriesJson) {
-      if ((deliveriesJson[key]["match_id"] >= matchNoStart16) && (deliveriesJson[key]["match_id"] <= matchNoEnd16)) {
-        yearDeliveriesData.push(deliveriesJson[key]);
-      }
-    }
-    //feed into extraRunsPerTeam
-    yearDeliveriesData.forEach(elem => {
-      let bowlingTeam = elem["bowling_team"];
-      let extraRuns = parseInt(elem["extra_runs"]);
-      if (bowlingTeam in extraRunsPerTeam) {
-        extraRunsPerTeam[bowlingTeam] = extraRunsPerTeam[bowlingTeam] + extraRuns;
-      } else {
-        extraRunsPerTeam[bowlingTeam] = extraRuns;
-      }
-    });
-
-    let dataFeed = [];
-    for (const key in extraRunsPerTeam) {
-      dataFeed.push({ "name": key, "y": extraRunsPerTeam[key] })
-    }
-
-    // Create the chart
-    Highcharts.chart('container', {
-      chart: { type: 'column' },
-      title: { text: 'IPL Analysis' },
-      subtitle: { text: 'Extras per Team (in 2016)' },
-      yAxis: { title: { text: 'Runs' } },
-      xAxis: {
-        categories: Object.keys(extraRunsPerTeam),
-        title: { text: "Team" }
-      },
-      plotOptions: {
-        series: {
-          borderWidth: 0,
-          dataLabels: {
-            enabled: true,
-            format: '{point.y:%f}'
-          }
+    let extraRunsPerTeam = deliveriesJson.reduce((acc, elem) => {
+      if ((elem["match_id"] >= matchNoStart16) && (elem["match_id"] <= matchNoEnd16)) {
+        let bowlingTeam = elem["bowling_team"];
+        let extraRuns = parseInt(elem["extra_runs"]);
+        if (bowlingTeam in acc) {
+          acc[bowlingTeam] += extraRuns;
+        } else {
+          acc[bowlingTeam] = extraRuns;
         }
-      },
-      "series": [
-        {
-          "data": dataFeed,
-          name: "Runs",
-          colorByPoint: true,
-          showInLegend: false
-        }
-      ]
+      }
+      return acc;
+    }, {});
+
+    let dataFeed = Object.keys(extraRunsPerTeam).map(year => {
+      return { "name": year, "y": extraRunsPerTeam[year] };
     });
+    createChart(Object.keys(extraRunsPerTeam), dataFeed, "Team", "Runs", "Extras per Team (in 2016)");
   })
 
   $('#show-bowler-economy').click(function () {
-    let bowlersEconomy = {};
-    let matchNoStart15;
-    let matchNoEnd15;
-    let yearDeliveriesData = [];
-    let sum = 0;
-    let matchPerSeason = {};
+    let matchPerSeason = getMatchSeason();
 
-    matchesJson.forEach(elem => {
-      let year = elem.season;
-      if (year in matchPerSeason) {
-        matchPerSeason[year] = matchPerSeason[year] + 1;
-      } else {
-        matchPerSeason[year] = 1;
-      }
-    });
-
-    for (const yr in matchPerSeason) {
-      if (yr <= 2015) {
-        sum += matchPerSeason[yr];
-      }
-    }
+    let sum = Object.keys(matchPerSeason).reduce((sum, year) => {
+      if (year <= 2015) return sum + matchPerSeason[year];
+      return (year <= 2015) ? sum + matchPerSeason[year] : sum;
+    }, 0)
     //since last season(2017) is at the top, Add 2017 matches to sum
     // replace 2017 with current season for other times
     sum += matchPerSeason[2017];
-    matchNoEnd15 = sum;
-    matchNoStart15 = sum - matchPerSeason[2015] + 1;
+    let matchNoEnd15 = sum;
+    let matchNoStart15 = sum - matchPerSeason[2015] + 1;
 
-    for (const key in deliveriesJson) {
-      if ((deliveriesJson[key]["match_id"] >= matchNoStart15) && (deliveriesJson[key]["match_id"] <= matchNoEnd15)) {
-        yearDeliveriesData.push(deliveriesJson[key]);
-      }
-    }
-    //feed into bowlersEconomy
-    yearDeliveriesData.forEach(elem => {
-      let bowler = elem["bowler"];
-      let runs = parseInt(elem["wide_runs"]) + parseInt(elem["noball_runs"]) + parseInt(elem["batsman_runs"]);
-      if (bowler in bowlersEconomy) {
-        bowlersEconomy[bowler]["runs"] += runs;
-        if (!Number(elem["wide_runs"]) && !Number(elem["noball_runs"])) {
-          bowlersEconomy[bowler]["balls"] += 1;
+    let bowlersEconomy = deliveriesJson.reduce((acc, elem) => {
+      if ((elem["match_id"] >= matchNoStart15) && (elem["match_id"] <= matchNoEnd15)) {
+        let bowler = elem["bowler"];
+        let runs = parseInt(elem["wide_runs"]) + parseInt(elem["noball_runs"]) + parseInt(elem["batsman_runs"]);
+        if (bowler in acc) {
+          acc[bowler]["runs"] += runs;
+          if (!Number(elem["wide_runs"]) && !Number(elem["noball_runs"])) {
+            acc[bowler]["balls"] += 1;
+          }
+          acc[bowler]["economy"] = (acc[bowler]["runs"] / acc[bowler]["balls"]) * 6;
+        } else {
+          acc[bowler] = {};
+          acc[bowler]["runs"] = runs;
+          acc[bowler]["balls"] = (!Number(elem["wide_runs"]) && !Number(elem["noball_runs"])) ? 1 : 0;
+          acc[bowler]["economy"] = (acc[bowler]["runs"] / acc[bowler]["balls"]) * 6;
         }
-        bowlersEconomy[bowler]["economy"] = (bowlersEconomy[bowler]["runs"] / bowlersEconomy[bowler]["balls"]) * 6;
-      } else {
-        bowlersEconomy[bowler] = {};
-        bowlersEconomy[bowler]["runs"] = runs;
-        bowlersEconomy[bowler]["balls"] = (!Number(elem["wide_runs"]) && !Number(elem["noball_runs"])) ? 1 : 0;
-        bowlersEconomy[bowler]["economy"] = (bowlersEconomy[bowler]["runs"] / bowlersEconomy[bowler]["balls"]) * 6;
       }
-    });
+      return acc;
+    }, {});
 
     let dataFeed = [];
     for (const key in bowlersEconomy) {
@@ -338,107 +269,53 @@ $(document).ready(() => {
     dataFeed.sort(function (a, b) {
       return a.y - b.y
     });
+
     let sortedNames = dataFeed.map(obj => {
       return obj.name;
     });
 
-    // Create the chart
-    Highcharts.chart('container', {
-      chart: { type: 'column' },
-      title: { text: 'IPL Analysis' },
-      subtitle: { text: 'Economical Bowlers (Top 15 in 2015 : min 10 overs)' },
-      yAxis: { title: { text: 'Economy' } },
-      xAxis: {
-        categories: sortedNames,
-        title: { text: "Bowler" }
-      },
-      plotOptions: {
-        series: {
-          borderWidth: 0,
-          dataLabels: {
-            enabled: true,
-            format: '{point.y:.3f}'
-          }
-        }
-      },
-      "series": [
-        {
-          "data": dataFeed.slice(0, 15),
-          name: "Economy",
-          colorByPoint: true,
-          showInLegend: false
-        }
-      ]
-    });
+    createChart(sortedNames, dataFeed.slice(0, 15), "Bowlers", "Economy", "Economical Bowlers (Top 15 in 2015 : min 10 overs)", '{point.y:.2f}');
   })
 
   $('#show-high-ind-score').click(function () {
-    let batsmenHighScore = {};
-    let matchData = {};
-
-    deliveriesJson.forEach(key => {
-      let matchId = key.match_id;
-      let batsman = key.batsman;
-      let run = parseInt(key.batsman_runs);
-      if (matchId in matchData) {
-        if (batsman in matchData[matchId]) {
-          matchData[matchId][batsman] += run;
+    let matchData = deliveriesJson.reduce((acc, delv) => {
+      let matchId = delv.match_id;
+      let batsman = delv.batsman;
+      let run = parseInt(delv.batsman_runs);
+      if (matchId in acc) {
+        if (batsman in acc[matchId]) {
+          acc[matchId][batsman] += run;
         } else {
-          matchData[matchId][batsman] = run;
+          acc[matchId][batsman] = run;
         }
       } else {
-        matchData[matchId] = {};
-        matchData[matchId][batsman] = run;
+        acc[matchId] = {};
+        acc[matchId][batsman] = run;
       }
-    });
+      return acc;
+    }, {});
 
-    for (const key in matchData) {
-      let teamBatsmen = Object.keys(matchData[key]);
-      teamBatsmen.forEach(bat => {
-        if (bat in batsmenHighScore) {
-          if (batsmenHighScore[bat] < matchData[key][bat]) {
-            batsmenHighScore[bat] = matchData[key][bat];
+    let batsmenHighScore = Object.keys(matchData).reduce((acc, match) => {
+      Object.keys(matchData[match]).forEach(batsman => {
+        if (batsman in acc) {
+          if (acc[batsman] < matchData[match][batsman]) {
+            acc[batsman] = matchData[match][batsman];
           }
         } else {
-          batsmenHighScore[bat] = matchData[key][bat];
+          acc[batsman] = matchData[match][batsman];
         }
       });
-    }
+      return acc;
+    }, {});
 
     let sortedNames = Object.keys(batsmenHighScore).sort(function (a, b) {
       return batsmenHighScore[b] - batsmenHighScore[a];
     });
+
     let sortedScore = sortedNames.map(elem => {
       return batsmenHighScore[elem];
     });
 
-    // Create the chart
-    Highcharts.chart('container', {
-      chart: { type: 'column' },
-      title: { text: 'IPL Analysis' },
-      subtitle: { text: 'Highest Individual Score (Top 15)' },
-      yAxis: { title: { text: 'Runs' } },
-      xAxis: {
-        categories: sortedNames.slice(0, 15),
-        title: { text: "Batsman" }
-      },
-      plotOptions: {
-        series: {
-          borderWidth: 0,
-          dataLabels: {
-            enabled: true,
-            format: '{point.y:%f}'
-          }
-        }
-      },
-      "series": [
-        {
-          "data": sortedScore.slice(0, 15),
-          name: "Runs",
-          colorByPoint: true,
-          showInLegend: false
-        }
-      ]
-    });
+    createChart(sortedNames.slice(0, 15), sortedScore.slice(0, 15), "Batsman", "Runs", "Highest Individual Score (Top 15)");
   })
 });
